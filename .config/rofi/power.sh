58f43875ee5e9b0545d593f3d049ae8e9027f7f7
koiking129 <cr2332rc@outlook.com>
2023-03-04 15:19:37 +0800

    Add a power menu script invoked by Rofi

---
+++ .config/rofi/power.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+args=($@)
+
+[ -n "$args" ] && selection="${args[-1]}"
+[ "$selection" = "Cancel" ] && exit
+
+if [ "${args[0]}" = "OK" ]; then
+    comfirmed="true"
+else
+    comfirmed="false"
+fi
+
+OPTIONS=("Shutdown" "Reboot" "Suspend" "Lock" "Quit")
+
+declare -A cmd
+cmd[Shutdown]="shutdown -h now"
+cmd[Reboot]="reboot"
+cmd[Suspend]="systemctl suspend"
+cmd[Lock]="$HOME/.local/bin/betterls"
+cmd[Quit]="pkill dwm"
+# cmd[Cancel]="exit"
+
+"$comfirmed" && exec ${cmd["$selection"]}
+
+if [ -z "$@" ]; then
+    for opt in "${OPTIONS[@]}"; do echo "${opt}"; done
+else
+    echo "OK $selection"
+    echo "Cancel"
+fi
