82609da463f52272e0eccf5cffc98552d3e7d00a
koiking129 <cr2332rc@outlook.com>
2023-11-21 19:52:13 +0800

    Use relative line numbers only in visual mode

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,6 +1,10 @@
 -- Line number
 vim.o.number = true
-vim.o.relativenumber = true
+-- [Use relative line numbers when entering visual mode](https://neovim.io/doc/user/autocmd.html#ModeChanged)
+vim.cmd("au ModeChanged [vV\x16]*:* let &l:rnu = mode() =~# '^[vV\x16]'")
+vim.cmd("au ModeChanged *:[vV\x16]* let &l:rnu = mode() =~# '^[vV\x16]'")
+vim.cmd("au WinEnter,WinLeave * let &l:rnu = mode() =~# '^[vV\x16]'")
+-- `\x16`: refer to [this](https://en.wikipedia.org/wiki/List_of_Unicode_characters#Control_codes)
 
 -- Indent
 vim.o.tabstop = 4  -- tab character
