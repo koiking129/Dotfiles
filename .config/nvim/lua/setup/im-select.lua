1a2f96ef7217a364220fd364d71125d97d8d16f0
koiking129 <cr2332rc@outlook.com>
2023-11-28 12:32:16 +0800

    Switch from fcitx to fcitx5

--- .config/nvim/lua/setup/im-select.lua
+++ .config/nvim/lua/setup/im-select.lua
@@ -3,20 +3,10 @@ local M = {}
 
 M.config = function ()
   require('im_select').setup({
+    default_im_select  = "keyboard-us",
+    default_command = 'fcitx5-remote',
 
-    -- IM will be set to `default_im_select` in `normal` mode
-    -- For Windows/WSL, default: "1033", aka: English US Keyboard
-    -- For macOS, default: "com.apple.keylayout.ABC", aka: US
-    -- For Linux, default: "keyboard-us" for Fcitx5 or "1" for Fcitx
-    -- You can use `im-select` or `fcitx5-remote -n` to get the IM's name you preferred
-    default_im_select  = "1",
 
-    -- Can be binary's name or binary's full path,
-    -- e.g. 'im-select' or '/usr/local/bin/im-select'
-    -- For Windows/WSL, default: "im-select.exe"
-    -- For macOS, default: "im-select"
-    -- For Linux, default: "fcitx5-remote" or "fcitx-remote"
-    default_command = 'fcitx-remote',
 
     -- Restore the default input method state when the following events are triggered
     set_default_events = { "VimEnter", "FocusGained", "InsertLeave", "CmdlineLeave" },
