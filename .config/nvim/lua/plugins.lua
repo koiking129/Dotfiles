690c812e578f9dca50280ad1a6e5f6dcc52b7d59
koiking129 <cr2332rc@outlook.com>
2023-12-27 16:41:37 +0800

    Install the nvim-surround

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -122,6 +122,23 @@ require('lazy').setup({
     config = require("setup/vim-floaterm").config,
   },
 
+  -- Surroundings
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    opts = {},
+  },
+  {
+    "roobert/surround-ui.nvim",
+    dependencies = {
+      "kylechui/nvim-surround",
+      "folke/which-key.nvim",
+    },
+    opts = {
+      root_key = "s",  -- Triggered by <leader>s
+    }
+  },
   -- Miscellaneous
   {
     "dstein64/vim-startuptime",
