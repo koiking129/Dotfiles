162fa34f71fd5350e7c291bcef2d2e6aa8c9de19
koiking129 <cr2332rc@outlook.com>
2023-12-27 11:55:58 +0800

    Config the vim-floaterm
    Add keymaps for toggling the floaterm
    Tweak the size of floaterm

--- .config/nvim/lua/setup/vim-floaterm.lua
+++ .config/nvim/lua/setup/vim-floaterm.lua
@@ -2,7 +2,13 @@ local M = {}
 
 
 M.config = function ()
-  vim.keymap.set('n', '<Leader>ft', ':FloatermNew <CR>')
+  -- Toggle the floaterm
+  vim.keymap.set('n', '<a-q>', ':FloatermToggle <CR>')
+  vim.keymap.set('t', '<a-q>', '<c-\\><c-n> :FloatermHide <CR>')
+
+  -- The relative size of floaterm
+  vim.g.floaterm_width = 0.8
+  vim.g.floaterm_height = 0.8
 end
 
 
