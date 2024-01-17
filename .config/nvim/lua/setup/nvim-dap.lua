76992adaaa76d4edb50b12e12e73f0361509a3c8
koiking129 <cr2332rc@outlook.com>
2024-01-17 21:42:07 +0800

    Add some keymaps to control the dap

--- .config/nvim/lua/setup/nvim-dap.lua
+++ .config/nvim/lua/setup/nvim-dap.lua
@@ -1,6 +1,16 @@
 local M = {}
 
 
+M.compile = function ()
+  local source_ftime = vim.fn.getftime(vim.fn.expand('%'))
+  local bin_ftime = vim.fn.getftime(vim.fn.expand('%:r'))
+
+  -- Do compile only if the source file was changed after the binary file
+  if (source_ftime > bin_ftime) then
+    vim.cmd("!g++ -g -o %:r %")
+  end
+end
+
 M.config = function ()
   local dap = require("dap")
 
@@ -28,6 +38,18 @@ M.config = function ()
       },
     },
   }
+
+  -- Change the directory -> Compile -> Start the debugger
+  vim.keymap.set("n", "<leader>dr", [[:cd %:h <CR>
+                                      :lua require('setup/nvim-dap').compile() <CR>
+                                      :DapContinue <CR>]])
+  vim.keymap.set("n", "<leader>db", ":DapToggleBreakpoint <CR>")
+  vim.keymap.set("n", "<leader>dC", ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) <CR>")
+  vim.keymap.set("n", "<leader>dc", ":DapContinue <CR>")
+  vim.keymap.set("n", "<leader>di", ":DapStepInto <CR>")
+  vim.keymap.set("n", "<leader>do", ":DapStepOut <CR>")
+  vim.keymap.set("n", "<leader>dn", ":DapStepOver <CR>")
+  vim.keymap.set("n", "<leader>dt", ":DapTerminate <CR>")
 end
 
 
