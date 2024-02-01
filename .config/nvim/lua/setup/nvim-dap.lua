ff025bb20ac75439895cb46fda966250d84050ad
koiking129 <cr2332rc@outlook.com>
2024-02-01 19:38:06 +0800

    feat(neovim): add the debug support for Rust projects

--- .config/nvim/lua/setup/nvim-dap.lua
+++ .config/nvim/lua/setup/nvim-dap.lua
@@ -1,21 +1,57 @@
 local M = {}
 
 
-M.compile = function ()
-  local source_ftime = vim.fn.getftime(vim.fn.expand('%'))
-  local bin_ftime = vim.fn.getftime(vim.fn.expand('%:r'))
-
-  -- Do compile only if the source file was changed after the binary file
-  if (source_ftime > bin_ftime) then
-    vim.cmd("!g++ -g -o %:r %")
+-- Generate the binary file
+local compile = function ()
+  if vim.fn.findfile('Cargo.toml') ~= "" then
+    -- Rust project
+    vim.cmd("!cargo build")
+  else
+    -- Single file
+    local source_ftime = vim.fn.getftime(vim.fn.expand('%'))
+    local bin_ftime = vim.fn.getftime(vim.fn.expand('%:r'))
+    -- Do compile only if the source file was changed after the binary file
+    if source_ftime > bin_ftime then
+      vim.cmd("!g++ -g -o '%:r' '%'")
+    end
   end
 end
+
+
+-- Start (Restart) debugging
+local dap_run = function ()
+  local dap = require("dap")
+
+  -- Stop the current session if exists
+  if dap.session() then
+    vim.cmd('DapTerminate')
+    vim.cmd('sleep 100ms')  -- OPTIM:
+  end
+
+  compile()
+
+  -- Change directory for competitive programming
+  local cp_home = vim.fn.expand("~/cp")
+  local wd = vim.fn.getcwd()
+  if wd == cp_home then
+    vim.cmd("cd %:h")
+  end
+
+  -- Start debugging
+  vim.cmd("DapContinue")
+
+  -- Restore the working directory
+  if string.find(vim.fn.getcwd(), cp_home) then
+    vim.cmd("cd " .. wd)
+  end
+end
+
 
 M.config = function ()
   local dap = require("dap")
 
   -- Use vscode-cpptools
-  local debugger_bin = vim.fn.expand('$HOME/.config/nvim/cpptools/extension/debugAdapters/bin/OpenDebugAD7')
+  local debugger_bin = '/opt/cpptools/extension/debugAdapters/bin/OpenDebugAD7'
 
   dap.adapters.cppdbg = {
     id = 'cppdbg',
@@ -28,9 +64,17 @@ M.config = function ()
       type = "cppdbg",
       request = "launch",
       program = function ()  -- The Path to the binary file
-        return vim.fn.expand('%:r')
+        local project_name = string.match(vim.fn.getcwd(), "%w+$")
+        if vim.fn.findfile('Cargo.toml') ~= "" then
+          -- The rust project
+          return vim.fn.getcwd() .. "/target/debug/" .. project_name
+        else
+          -- Single file
+          return vim.fn.expand('%:r')
+        end
       end,
       cwd = '${workspaceFolder}',
+      stopAtEntry = true,
       setupCommands = {
         {  -- Show the contents of STL containers
            text = '-enable-pretty-printing',
@@ -41,19 +85,20 @@ M.config = function ()
     },
   }
 
-  -- Change the directory -> Compile -> Start the debugger
-  vim.keymap.set("n", "<leader>dr", [[:cd %:h <CR>
-                                      :lua require('setup/nvim-dap').compile() <CR>
-                                      :DapContinue <CR>]])
-  vim.keymap.set("n", "<leader>db", ":DapToggleBreakpoint <CR>")
-  vim.keymap.set("n", "<leader>dC", ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) <CR>")
+  dap.configurations.rust = dap.configurations.cpp
+
+  vim.keymap.set("n", "<leader>dr", dap_run)
   vim.keymap.set("n", "<leader>dc", ":DapContinue <CR>")
+  vim.keymap.set("n", "<leader>dC", dap.run_to_cursor)
+  vim.keymap.set("n", "<leader>db", ":DapToggleBreakpoint <CR>")
+  vim.keymap.set("n", "<leader>dB", function()
+    dap.set_breakpoint(vim.fn.input('Conditional breakpoint: '))
+  end)
+  vim.keymap.set("n", "<leader>dn", ":DapStepOver <CR>")
   vim.keymap.set("n", "<leader>di", ":DapStepInto <CR>")
   vim.keymap.set("n", "<leader>do", ":DapStepOut <CR>")
-  vim.keymap.set("n", "<leader>dn", ":DapStepOver <CR>")
   vim.keymap.set("n", "<leader>dt", ":DapTerminate <CR>")
 end
 
 
 return M
-
