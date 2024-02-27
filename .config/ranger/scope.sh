a70b756d3fc9d1d7015acbfa80cf5cdbde3557b3
koiking129 <cr2332rc@outlook.com>
2024-02-27 21:22:40 +0800

    fix(ranger): preview `.zip` file via 7z

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -51,7 +51,7 @@ handle_extension() {
     case "${FILE_EXTENSION_LOWER}" in
         ## Archive
         a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
-        rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
+        rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z)
             atool --list -- "${FILE_PATH}" && exit 5
             bsdtar --list --file "${FILE_PATH}" && exit 5
             exit 1;;
@@ -59,7 +59,7 @@ handle_extension() {
             ## Avoid password prompt by providing empty password
             unrar lt -p- -- "${FILE_PATH}" && exit 5
             exit 1;;
-        7z)
+        7z|zip)
             ## Avoid password prompt by providing empty password
             7z l -p -- "${FILE_PATH}" && exit 5
             exit 1;;
