8c94649c0fc5ade01a7ca115b7c9a36929741621
koiking129 <cr2332rc@outlook.com>
2024-05-01 17:36:01 +0800

    feat(neovim): add some keymaps for fzf.vim

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -6,9 +6,15 @@ return {
     require("which-key").add({ "<leader>f", group = "fzf" })
   end,
   keys = {
-    { '<leader>ff', '<CMD>Files<CR>',   desc = "Files" }, -- runs $FZF_DEFAULT_COMMAND
-    { '<leader>fa', '<CMD>Ag<CR>',      desc = "Text" },
-    { '<leader>fb', '<CMD>Buffers<CR>', desc = "Buffers" },
-    { '<leader>fg', '<CMD>GFiles?<CR>', desc = "Git" },
+    { '<leader>ff', '<CMD>Files<CR>',    desc = "Files" }, -- runs $FZF_DEFAULT_COMMAND
+    { '<leader>fh', '<CMD>History<CR>',  desc = "History" },
+    { '<leader>fa', '<CMD>Ag<CR>',       desc = "Text" },
+    { '<leader>fl', '<CMD>Lines<CR>',    desc = "Lines" },
+    { '<leader>fb', '<CMD>Buffers<CR>',  desc = "Buffers" },
+    { '<leader>fg', '<CMD>GFiles?<CR>',  desc = "Git" },
+    { '<leader>f:', '<CMD>History:<CR>', desc = "Command history" },
+    { '<leader>fc', '<CMD>Commands<CR>', desc = "Commands" },
+    { '<leader>fC', '<CMD>Colors<CR>',   desc = "Color schemes" },
+    { '<leader>fm', '<CMD>Maps<CR>',     desc = "Normal mode mappings" },
   },
 }
