f60ab7e4a85e3cce89c9bb183ad78d802f4a597c
koiking129 <cr2332rc@outlook.com>
2024-05-06 00:43:08 +0800

    fix(ranger): don't preview large archives

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -52,6 +52,7 @@ handle_extension() {
         ## Archive
         a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
         rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z)
+            [ "$(du -s "${FILE_PATH}" | awk '{print $1}')" -gt 500000 ] && exit 1
             atool --list -- "${FILE_PATH}" && exit 5
             bsdtar --list --file "${FILE_PATH}" && exit 5
             exit 1;;
